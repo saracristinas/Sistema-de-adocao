a08929d7f951f831f07983bffc2f58b5bc627814
saracristinas <sarasales17062000@gmail.com>
2025-05-07 17:55:22 -0300

    Adicionando imagens no form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,76 +4,71 @@ import { useState } from "react";
 
 import "./Login.css";
 
-// import meninaAnimais from "../../assets/menina-e-animais.webp";
-// import patinhas from "../../assets/patinhas.webp";
-// import pc from "../../assets/pc.png";
-// import rodinha from "../../assets/rodinha.webp";
+import meninaImg from '../../assets/menina-e-animais.webp';
+import patinhas from '../../assets/patinhas.webp';
+import pc from '../../assets/pc.png';
 
 
 const Login = () => {
-
-    const[username, setUsername] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassoword] = useState("");
-
+  
     const handlerSubimit = (event) => {
-        event.preventDefault();
-
-        // ### Console direto na aplicacao pra verificar se os dados estao sendo carregados no front
-        // console.log(username, password);
-
-        // console.log("Envio");
-
-        alert("Enviando os dados: " + "Email: " + username + " - " + "Senha: "+ password);
+      event.preventDefault();
+      alert("Enviando os dados: " + "Email: " + username + " - " + "Senha: "+ password);
     };
+  
+    return (
+<div className="login-wrapper">
+    
+  <div className="left-side">
+    <div className="image-container">
+      <img src={meninaImg} alt="Menina com animais" className="img-menina" />
+      <img src={patinhas} alt="patinhas de cachorro" className="img-patinha" />
+      <img src={pc} alt="notebook" className="img-pc" />
+    </div>
+  </div>
 
-  return (
-      <div className="container">
-        <form onSubmit={handlerSubimit}>
-            <h1>Login</h1>
-            <div className="input-field">
-                <input 
-                type="email" 
-                placeholder='E-mail' 
-                required /* Nao permite que o usuario entre sem email*/
-                onChange={(e) => setUsername(e.target.value)}
-                />
-                <FaUser className="icon" />
-            </div>
-
-            <div className="input-field">
-                <input 
-                type="password" 
-                placeholder='Senha' 
-                required /* Nao permite que o usuario entre sem senha*/
-                onChange={(e) => setPassoword(e.target.value)}/>
-                <FaLock className="icon" />
-            </div>
-
-            <div className="recall-forget">
-                <label>
-                    <input type="checkbox" />
-                    Lembre de mim
-                </label>
-                <a href="#">Esqueceu sua senha?</a>
-            </div>
-            <button>Entrar</button>
-
-            <div className="signup-link">
-                <p>
-                    Não tem uma conta? <a href="#">Cadastrar</a>
-                </p>
-            </div>
-        </form>
-
-        {/* <div className="imagens">
-            <img src="{menina-e-animais.webp}" alt="menina com animais" className="img-menina" />
-            <img src="{patinhas}" alt="patinhas de cachorro na tela" className="img-patinhas" />
-            <img src="{pc}" alt="computador" className="img-pc" />
-            <img src="{rodinha}" alt="rodinha de patinhas" className="img-rodinha" />
-        </div> */}
-
-      </div>
-  )
-}
-
-export default Login
+  <div className="right-side">
+    <div className="form-container">
+      <form onSubmit={handlerSubimit}>
+        <h1>Login</h1>
+        <div className="input-field">
+          <input 
+            type="email" 
+            placeholder='E-mail' 
+            required 
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+          <input 
+            type="password" 
+            placeholder='Senha' 
+            required 
+            onChange={(e) => setPassoword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu sua senha?</a>
+        </div>
+        <button>Entrar</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Cadastrar</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+    );
+  };
+  
+  export default Login;  
